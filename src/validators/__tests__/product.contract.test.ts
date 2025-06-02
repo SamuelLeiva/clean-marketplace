5204c0418b6972e66934270727aa6313239297d3
SamuelLeiva <leiva20152@gmail.com>
2025-06-02 11:30:20 -0500

    Tests' configuration and first mock added

---
+++ src/validators/__tests__/product.contract.test.ts
@@ -0,0 +1,28 @@
+import { CreateProductInput } from '@/contracts/product.contract'
+import { describe, it, expect } from 'vitest'
+
+// Mocks simples para validar los contratos
+describe('CreateProductInput', () => {
+  it('rejects invalid input', () => {
+    const result = CreateProductInput.safeParse({
+      name: 'A',
+      price: -10,
+      categoryId: 'invalid-uuid',
+    })
+
+    expect(result.success).toBe(false)
+    if (!result.success) {
+      expect(result.error.errors.length).toBeGreaterThan(0)
+    }
+  })
+
+  it('accepts valid input', () => {
+    const result = CreateProductInput.safeParse({
+      name: 'Guitar',
+      price: 200,
+      categoryId: '9e1a8be0-5dfb-4b44-a154-25ae4721f120',
+    })
+
+    expect(result.success).toBe(true)
+  })
+})
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  //infiere los paths usando alias @ que definimos en ts.config
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
+    setupFiles: [],
+  },
+})
