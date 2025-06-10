6a0f8b1b9e9f5e5d49f683fe8bd64e00b6140d48
SamuelLeiva <leiva20152@gmail.com>
2025-06-10 11:13:01 -0500

    Chore: Added initial category.contract

---
+++ src/shared/contracts/category.contract.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod'
+
+const BaseCategorySchema = z.object({
+  name: z
+    .string()
+    .min(3, 'Name must be at least 3 characters')
+    .max(100, 'Name must be at most 100 characters'),
+  description: z
+    .string()
+    .min(10, 'Description must be at least 10 characters')
+    .max(500, 'Name must be at most 500 characters')
+    .optional(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
+})
+
+export const CreateCategoryInput = BaseCategorySchema.omit({
+  createdAt: true,
+  updatedAt: true,
+})
+
+export const UpdateCategoryInput = BaseCategorySchema.partial().omit({
+  createdAt: true,
+  updatedAt: true,
+})
+
+export const CategoryResponse = BaseCategorySchema.extend({
+  id: z.string().uuid(),
+}).omit({
+  createdAt: true,
+  updatedAt: true,
+})
+
+export const CategoryListResponse = z.array(CategoryResponse)
+
+//Tipos inferidos
+export type CreateCategoryInput = z.infer<typeof CreateCategoryInput>
+export type UpdateCategoryInput = z.infer<typeof UpdateCategoryInput>
+export type CategoryResponse = z.infer<typeof CategoryResponse>
+export type CategoryListResponse = z.infer<typeof CategoryListResponse>
