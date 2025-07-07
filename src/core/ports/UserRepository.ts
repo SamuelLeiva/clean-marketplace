76fe8f35e275ba1f58a8a40ad6ae56f45865f9c5
SamuelLeiva <leiva20152@gmail.com>
2025-07-07 11:57:15 -0500

    chore: user's repository added

--- src/core/ports/UserRepository.ts
+++ src/core/ports/UserRepository.ts
@@ -1,14 +1,8 @@
-import { CreateUserInput, UpdateUserInput } from '@/shared/contracts'
 import { User } from '../entities'
+import { SignUpInput } from '@/shared/contracts';
 
 export interface UserRepository {
-  create(user: CreateUserInput): Promise<User>
   findById(id: string): Promise<User | null>
-  findAll(): Promise<User[]>
-  update(id: string, user: UpdateUserInput): Promise<User>
-  delete(id: string): Promise<void>
-
-  // Métodos adicionales al CRUD clásico
-  findByName(name: string): Promise<User | null>
-  isInUse(id: string): Promise<boolean>
+  findByEmail(email: string): Promise<User | null>
+  create(user: SignUpInput): Promise<User>
 }
--- src/shared/contracts/user.contract.ts
+++ src/shared/contracts/user.contract.ts
@@ -8,6 +8,7 @@ const BaseUserSchema = z.object({
   updatedAt: z.string().datetime(),
 }).strict();
 
+
 // Esquema para la entrada de registro (signup)
 export const SignUpInput = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters').max(50, 'Name must be at most 50 characters').optional(),
